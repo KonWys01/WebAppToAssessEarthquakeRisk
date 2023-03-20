9d0a8ec7af16b4b5ab31c5790e8082de4bb5d1b2
KonWys01 <konradwysokinski01@gmail.com>
2023-03-20 21:48:48 +0100

    init

---
+++ main.py
@@ -0,0 +1,36 @@
+import requests
+from time import sleep
+from datetime import timedelta, date
+
+
+def get_latest_date():
+    pass
+
+
+def daterange(start_date, end_date, step = 1):
+    if step == 1:
+        for n in range(int((end_date - start_date).days)):
+            yield start_date + timedelta(n)
+    else:
+        for n in range(int((start_date - end_date).days)):
+            yield start_date - timedelta(n)
+
+
+def download_data(url: str):
+    for single_date in daterange(date(2023, 3, 19), date(2022, 3, 19), -1):
+        try:
+            earthquake = requests.get(url + f'&starttime={single_date}&endtime={single_date + timedelta(1)}')
+            print(f'earthquake for {single_date} was downloaded')
+            data = earthquake.content
+            with open(f'{single_date.day}-{single_date.month}-{single_date.year}.geojson', 'wb') as f:
+                # geojson.dump(earthquake, f)
+                f.write(data)
+            sleep(10)
+        except:
+            print(f'{single_date}')
+            continue
+
+
+if __name__ == '__main__':
+    base_url = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson"
+    download_data(base_url)
