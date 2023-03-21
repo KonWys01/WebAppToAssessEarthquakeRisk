7dbc5181e04a3bf52575e7855ca324b9e66356de
KonWys01 <konradwysokinski01@gmail.com>
2023-03-21 16:42:27 +0100

    get latest date

--- main.py
+++ main.py
@@ -1,13 +1,23 @@
 import requests
 from time import sleep
-from datetime import timedelta, date
+from datetime import timedelta, date, datetime
+import glob
+import os
 
 
 def get_latest_date():
-    pass
+    path = f'{os.getcwd()}\\downloaded_data\\'
+    extension = f'.geojson'
+    files = glob.glob(path + '*' + extension)
+    dates = []
+    for file in files:
+        file_name = file.replace(path, '').replace(extension, '')
+        dates.append(datetime.strptime(file_name, '%d-%m-%Y').date())
+    youngest_date = min(dates)
+    return youngest_date
 
 
-def daterange(start_date, end_date, step = 1):
+def daterange(start_date, end_date, step=1):
     if step == 1:
         for n in range(int((end_date - start_date).days)):
             yield start_date + timedelta(n)
@@ -17,12 +27,14 @@ def daterange(start_date, end_date, step = 1):
 
 
 def download_data(url: str):
-    for single_date in daterange(date(2023, 3, 19), date(2022, 3, 19), -1):
+    starting_date = get_latest_date()
+
+    for single_date in daterange(starting_date, starting_date - timedelta(days=365), -1):
         try:
             earthquake = requests.get(url + f'&starttime={single_date}&endtime={single_date + timedelta(1)}')
             print(f'earthquake for {single_date} was downloaded')
             data = earthquake.content
-            with open(f'{single_date.day}-{single_date.month}-{single_date.year}.geojson', 'wb') as f:
+            with open(f'downloaded_data/{single_date.day}-{single_date.month}-{single_date.year}.geojson', 'wb') as f:
                 # geojson.dump(earthquake, f)
                 f.write(data)
             sleep(10)
