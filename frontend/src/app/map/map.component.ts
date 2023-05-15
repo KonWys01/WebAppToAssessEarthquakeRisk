39fb40cd7366ee3e8a6f592d969d3ae471d19083
KonWys01 <konradwysokinski01@gmail.com>
2023-05-15 23:07:40 +0200

    markers

--- frontend/src/app/map/map.component.ts
+++ frontend/src/app/map/map.component.ts
@@ -9,11 +9,13 @@ import * as L from 'leaflet';
 export class MapComponent implements OnInit {
   private centroid: L.LatLngExpression = [52.22, 21];
   private map: L.Map;
+  markers: L.Marker[] = [];
 
   constructor() {}
 
   ngOnInit(): void {
     this.initMap();
+    this.initMarkers();
   }
 
   initMap(): void {
@@ -30,6 +32,55 @@ export class MapComponent implements OnInit {
     );
 
     tiles.addTo(this.map);
+  }
+
+  initMarkers() {
+    const initialMarkers = [
+      {
+        position: { lat: 28.625485, lng: 79.821091 },
+        draggable: true,
+      },
+      {
+        position: { lat: 28.625293, lng: 79.817926 },
+        draggable: false,
+      },
+      {
+        position: { lat: 28.625182, lng: 79.81464 },
+        draggable: true,
+      },
+    ];
+    for (let index = 0; index < initialMarkers.length; index++) {
+      const data = initialMarkers[index];
+      const marker = this.generateMarker(data, index);
+      marker
+        .addTo(this.map)
+        .bindPopup(`<b>${data.position.lat},  ${data.position.lng}</b>`);
+      this.map.panTo(data.position);
+      this.markers.push(marker);
+    }
+  }
+
+  generateMarker(data: any, index: number) {
+    return L.marker(data.position, { draggable: data.draggable })
+      .on('click', (event: any) => this.markerClicked(event, index))
+      .on('dragend', (event: any) => this.markerDragEnd(event, index));
+  }
+
+  onMapReady($event: L.Map) {
+    this.map = $event;
+    this.initMarkers();
+  }
+
+  mapClicked($event: any) {
+    console.log($event.latlng.lat, $event.latlng.lng);
+  }
+
+  markerClicked($event: any, index: number) {
+    console.log($event.latlng.lat, $event.latlng.lng);
+  }
+
+  markerDragEnd($event: any, index: number) {
+    console.log($event.target.getLatLng());
   }
 }
 //   map!: L.Map;
