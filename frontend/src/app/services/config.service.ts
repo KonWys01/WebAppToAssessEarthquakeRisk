2afbc431e46271f3dc397160e0b92f1ddb2fc90d
KonWys01 <konradwysokinski01@gmail.com>
2023-11-03 21:48:49 +0100

    config service

---
+++ frontend/src/app/services/config.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { firstValueFrom } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ConfigService {
+  private appConfig: any;
+  constructor(public http: HttpClient) {}
+  async loadConfig(): Promise<void> {
+    await firstValueFrom(this.http.get('/assets/config.json')).then(
+      (config) => {
+        this.appConfig = config;
+      }
+    );
+  }
+
+  get apiBaseUrl(): string {
+    return this.appConfig.baseUrl;
+  }
+
+  get apiEarthquake(): string {
+    return this.appConfig.earthquake;
+  }
+}
