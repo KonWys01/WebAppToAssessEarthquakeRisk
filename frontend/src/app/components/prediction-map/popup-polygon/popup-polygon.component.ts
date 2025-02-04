ddd295718ff0247a31dafd327f0f06e15c7b07e1
KonWys01 <konradwysokinski01@gmail.com>
2025-02-04 15:36:46 +0100

    clean up popoup polygon

--- frontend/src/app/components/prediction-map/popup-polygon/popup-polygon.component.ts
+++ frontend/src/app/components/prediction-map/popup-polygon/popup-polygon.component.ts
@@ -22,37 +22,7 @@ export class PopupPolygonComponent implements OnInit {
 
   ngOnInit() {
     let data = this.predictionService.prediction_values(this.index)
-    // for (let i = 0; i < data.length; i++) {
-    let sum = 0;
-    let mean = 0;
-    let stdDev = 0;
-    
-    // Check if there are multiple zeros at the start
-    let hasMultipleStartingZeros = true;
-    for (let i = 0; i < Math.min(3, data.length); i++) {
-      if (data[i]['value'] !== 0) {
-        hasMultipleStartingZeros = false;
-        break;
-      }
-    }
-    
-    // Calculate mean
     for (let i = 0; i < data.length; i++) {
-      sum += data[i]['value'];
-    }
-    mean = sum / data.length;
-    
-    // Calculate standard deviation and only randomize if no multiple starting zeros
-    let squaredDiffs = 0;
-    for (let i = 0; i < data.length; i++) {
-      if (!hasMultipleStartingZeros) {
-        data[i]['value'] = Math.floor(data[i]['value']) + Math.floor(Math.random() * 3);
-        data[i]['value_round'] = Math.floor(data[i]['value_round']) + Math.floor(Math.random() * 3);
-      }
-      squaredDiffs += Math.pow(data[i]['value'] - mean, 2);
-    }
-    stdDev = Math.floor(Math.sqrt(squaredDiffs / data.length));
-    for (let i = 0; i < 95; i++) {
         let row = {
           'year': data[i]['date'].substring(0,4),
           'month': data[i]['date'].substring(5,7),
