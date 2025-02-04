c1218a6ef45354ca533d66951f980155fcca433f
KonWys01 <konradwysokinski01@gmail.com>
2025-02-04 16:06:58 +0100

    ml router

--- backend/routers/ml.py
+++ backend/routers/ml.py
@@ -8,17 +8,3 @@ machinelearning_router = APIRouter(
 @machinelearning_router.get("/{id}")
 async def get_prediction_per_polygon():
     return None
-    # crud_data = get_multiple_earthquakes(
-    #     db=db,
-    #     mag_min=mag_min,
-    #     mag_max=mag_max,
-    #     date_start=date_start,
-    #     date_end=date_end,
-    #     coordinates=coordinates,
-    #     type_eq=type
-    # )
-    # return ResponseModel(
-    #     data=crud_data,
-    #     status_code=status.HTTP_200_OK,
-    #     count=len(crud_data)
-    # )
